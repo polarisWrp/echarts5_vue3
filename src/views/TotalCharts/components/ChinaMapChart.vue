35c5eca9072624962b45bb7cb639467bcc566c15
Polaris29 <1977660457@qq.com>
2022-11-29 00:35:10 +0800

    feat: 按需引入echarts图表

--- src/views/TotalCharts/components/ChinaMapChart.vue
+++ src/views/TotalCharts/components/ChinaMapChart.vue
@@ -8,7 +8,6 @@
 
 <script setup lang="ts">
 import SocketService from '@/utils/useWebSocket';
-import * as $echarts from 'echarts';
 import { EChartsOption } from 'echarts';
 import {
   ref,
@@ -24,6 +23,36 @@ import { useStore } from '@/store/index';
 import axios from 'axios';
 import { UserData } from '@/types/charts';
 import { getProvinceMapInfo } from '@/utils/mapUtil';
+import * as $echarts from 'echarts/core';
+// 引入柱状图图表，图表后缀都为 Chart
+import { MapChart } from 'echarts/charts';
+// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  GeoComponent,
+} from 'echarts/components';
+// 标签自动布局，全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 注册必须的组件
+$echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  // GridComponent,
+  GeoComponent,
+  // DatasetComponent,
+  // TransformComponent,
+  MapChart,
+  // LabelLayout,
+  // UniversalTransition,
+  CanvasRenderer,
+]);
 
 const store = useStore();
 const mapRef = ref<HTMLDivElement>();
--- src/views/TotalCharts/components/RankChart.vue
+++ src/views/TotalCharts/components/RankChart.vue
@@ -12,7 +12,6 @@ import { useStore } from '@/store';
 import { RankData } from '@/types/charts';
 import { rankColorArr } from '@/types/charts/colorConfig';
 import SocketService from '@/utils/useWebSocket';
-import * as $echarts from 'echarts';
 import { EChartsOption } from 'echarts';
 import {
   ref,
@@ -24,6 +23,36 @@ import {
   watch,
   computed,
 } from 'vue';
+import * as $echarts from 'echarts/core';
+// 引入柱状图图表，图表后缀都为 Chart
+import { BarChart } from 'echarts/charts';
+// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  DataZoomComponent,
+} from 'echarts/components';
+// 标签自动布局，全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 注册必须的组件
+$echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DataZoomComponent,
+  // DatasetComponent,
+  TransformComponent,
+  BarChart,
+  // LabelLayout,
+  // UniversalTransition,
+  CanvasRenderer,
+]);
 
 const store = useStore();
 // dom节点
